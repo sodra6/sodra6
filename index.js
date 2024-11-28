1a142b192a8d4fc9b8e71a81ae727467554465f5
sodra6 <sodra6@naver.com>
2024-11-28 09:38:40 +0900

    feat : git action setting add

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+import { writeFileSync } from 'node:fs';
+import Parser from "rss-parser";
+
+/**
+ * README.MD에 작성될 페이지 텍스트
+ */
+let text = `
 <div align="center">
 <img src="https://capsule-render.vercel.app/api?type=waving&color=blue&height=100&section=header&text=I%20am%20Full-Stack%20Developer,%20YongGyu&fontSize=30"/>
 <br/>
@@ -40,3 +47,38 @@
 
  
 </div>
+
+## 📕 Latest Blog Posts
+
+`;
+
+// rss-parser 생성
+const parser = new Parser({
+    headers: {
+        Accept: 'application/rss+xml, application/xml, text/xml; q=0.1',
+    }});
+
+(async () => {
+
+    // 피드 목록
+    const feed = await parser.parseURL('https://devpad.tistory.com/rss'); // 본인의 블로그 주소
+    
+    text += `<ul>`;
+    
+    // 최신 10개의 글의 제목과 링크를 가져온 후 text에 추가
+    for (let i = 0; i < 10; i++) {
+        const {title, link} = feed.items[i];
+        console.log(`${i + 1}번째 게시물`);
+        console.log(`추가될 제목: ${title}`);
+        console.log(`추가될 링크: ${link}`);
+        text += `<li><a href='${link}' target='_blank'>${title}</a></li>`;
+    }
+
+    text += `</ul>`;
+    
+    // README.md 파일 생성
+    writeFileSync('README.md', text, 'utf8', (e) => {
+        console.log(e);
+    })
+    console.log('업데이트 완료');
+})();
