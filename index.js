7534bcdc34c1f6f5b1891ca6e19f9128e80cf15e
sodra6 <sodra6@naver.com>
2024-11-28 09:42:52 +0900

    feat : blog url modify

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const parser = new Parser({
 (async () => {
 
     // 피드 목록
-    const feed = await parser.parseURL('https://devpad.tistory.com/rss'); // 본인의 블로그 주소
+    const feed = await parser.parseURL('https://sodra6.tistory.com/rss'); // 본인의 블로그 주소
     
     text += `<ul>`;
     
